3cc9eb48742a7cea829a99fdaae79f899b329875
Michael Harris <mjh150@duke.edu>
2025-08-03 22:22:19 -0400

    Initial upload of bash scripts for MRI steps to cortical surface maps

---
+++ flatmountFS.sh
@@ -0,0 +1,116 @@
+#!bin/bash
+
+# flatmountFS.sh takes @animal_warper's output and performs a series of steps
+# using freesurfer's powerful cortical mesh tools, including generating spheroid 
+# and flatmap representations. Both are important for projection of
+# -omic or other 2D data collected and based off the subject brain.
+
+# As stated in flatmountPrep.sh, it is HIGHLY recommended to check @animal_warper output and perform manual
+# tweaks to segmentation masks using ITK-SNAP. This has been mentioned by both by both C Klink and R Boshra. 
+# It involves loading "main image" which is the brain mask for your subject, and loading the "segmentation" ("SEG") mask. 
+# Set opacity levels so both can be seen well, and essentially enjoy some painting time, as there will be voxels that need
+# be re-numbered according to what their actual values are. 
+
+# IMPORTANT: There is a change in value categories generated between use of the original segmenting procedure shown by C. Klink, 
+# which involved use of the NMT v1.2, and the NMT v2 procedure outlined here and by R. Boshra. NMT_v2 has two threshold values 
+# which get categorized as WM, ranging from 3 to 4, rather than just 3 as w/ the original NMT v1.2. 
+# In ITK-SNAP, you will notice some parts of the white matter are either a blue OR yellow color (values of 3 OR 4), respectively. 
+# In the below lines, I utilize numbering outlined by R. Boshra since we have been using the NMT v2.
+# SAVE THE MODIFIED VERSION AS A COPY! You'll see below I named mine "SEG_in_BONNIE_ITK.nii.gz" to specify the ITK-SNAP edit.
+
+
+SUBJ=BONNIE #Subject name
+startpath=$(pwd) # current working directory
+Template=NMT_v2.0_sym_05mm # Variable specifying the template name (folder containing the template should also have this name. This is how it is downloaded from AFNI)
+NMT_path=${startpath}/${Template} # NMT 0.5mm used in this project to avoid voxel resampling
+
+
+#travel to directory holding the segmented "in subject" scans
+cd ${NMT_path}/single_subject_scans/{$SUBJ}/AW/
+
+# On the copied segmentation file, create separate masks for each segmented category
+# WM, GM, CSF, etc.
+fslmaths SEG_in_${SUBJ}_ITK.nii.gz -thr 0.9 -uthr 1.1 -bin ${SUBJ}_CSF.nii.gz
+fslmaths SEG_in_${SUBJ}_ITK.nii.gz -thr 1.9 -uthr 2.1 -bin ${SUBJ}_GM.nii.gz
+fslmaths SEG_in_${SUBJ}_ITK.nii.gz -thr 2.9 -uthr 4.1 -bin ${SUBJ}_WM.nii.gz
+fslmaths SEG_in_${SUBJ}_ITK.nii.gz -thr 0.5 -uthr 4.1 -bin ${SUBJ}_brainmask.nii.gz
+
+# create folder for freesurfer output
+mkdir -p fsSurf 
+# make subfolders for structure
+mkdir -p fsSurf/src
+mkdir -p fsSurf/mgz
+mkdir -p fsSurf/temp
+
+# copy source files from NMT segmentation to the new freesurfer folders
+cp ${SUBJ}.nii.gz fsSurf/src/T1.nii.gz
+cp ${SUBJ}_ns.nii.gz fsSurf/src/brain.nii.gz
+cp ${SUBJ}_ns.nii.gz fsSurf/src/brainmask.nii.gz # freesurfer brainmask aren't actually masks
+cp ${SUBJ}_WM.nii.gz fsSurf/src/wm.nii.gz
+
+# make another directory that will be the "src" images but kept in an original state (headers will be changed to fake voxel dimensions).
+mkdir fsSurf/src/org
+cp fsSurf/src/*.nii.gz fsSurf/src/org/
+
+# change headers for faking the 1mm voxel dimensions
+3drefit -xdel 1.0 -ydel 1.0 -zdel 1.0 -keepcen fsSurf/src/T1.nii.gz &
+3drefit -xdel 1.0 -ydel 1.0 -zdel 1.0 -keepcen fsSurf/src/brain.nii.gz &
+3drefit -xdel 1.0 -ydel 1.0 -zdel 1.0 -keepcen fsSurf/src/brainmask.nii.gz &
+3drefit -xdel 1.0 -ydel 1.0 -zdel 1.0 -keepcen fsSurf/src/wm.nii.gz &
+
+#convert to .mgz format for freesurfer and place in the mgz folder
+mri_convert -c fsSurf/src/T1.nii.gz fsSurf/mgz/T1.mgz &
+mri_convert -c fsSurf/src/brain.nii.gz fsSurf/mgz/brain.mgz &
+mri_convert -c fsSurf/src/brain.nii.gz fsSurf/mgz/brainmask.mgz &
+mri_convert -c fsSurf/src/brainmask.nii.gz fsSurf/mgz/brainmask_binary.mgz &
+mri_convert -c fsSurf/src/wm.nii.gz fsSurf/mgz/wm.mgz &
+
+# create brain.finalsurfs.mgz
+mri_mask -T 5 fsSurf/mgz/brain.mgz fsSurf/mgz/brainmask.mgz fsSurf/mgz/brain.finalsurfs.mgz &
+
+# Inspect volume, get coordinates for corpus callosum and pons
+freeview -v fsSurf/mgz/brain.mgz &
+
+echo "Inspect brain volume in Freeview and get coordinates for the corpus callosum (CC) and pons (PONS) to continue."
+read -p "Enter CC coordinates as X Y Z (e.g., 127 104 134): " -a CC # Read user input into an array named CC
+read -p "Enter PONS coordinates as X Y Z (e.g., 127 153 126): " -a PONS # Read user input into an array named PONS
+read -p "Ready to continue? Press Enter to begin cortical surface generation" # Wait for user input
+
+#Fill WM
+mri_fill -CV ${CC[0]} ${CC[1]} ${CC[2]} \
+    -PV ${PONS[0]} ${PONS[1]} ${PONS[2]} \
+    fsSurf/mgz/wm.mgz fsSurf/mgz/filled.mgz
+
+# copy original white matter before applying fixes
+cp fsSurf/mgz/wm.mgz fsSurf/mgz/wm_nofix.mgz
+
+
+# Tesselate
+# left hemisphere
+mri_pretess fsSurf/mgz/filled.mgz 255 fsSurf/mgz/brain.mgz fsSurf/mgz/wm_filled-pretess255.mgz
+mri_tessellate fsSurf/mgz/wm_filled-pretess255.mgz 255 fsSurf/temp/lh.orig.nofix
+# right hemisphere
+mri_pretess fsSurf/mgz/filled.mgz 127 fsSurf/mgz/brain.mgz fsSurf/mgz/wm_filled-pretess127.mgz
+mri_tessellate fsSurf/mgz/wm_filled-pretess127.mgz 127 fsSurf/temp/rh.orig.nofix
+
+# Define hemispheres
+HEMI=(lh rh) # Array containing left and right hemispheres
+
+# for both hemispheres
+for xh in ${HEMI[@]}; do
+    # create a version we can edit
+    cp fsSurf/temp/${xh}.orig.nofix fsSurf/temp/${xh}.orig
+
+    # post-process tesselation
+    mris_extract_main_component fsSurf/temp/${xh}.orig.nofix fsSurf/temp/${xh}.orig.nofix
+    mris_smooth -nw fsSurf/temp/${xh}.orig.nofix fsSurf/temp/${xh}.smoothwm.nofix
+    mris_inflate fsSurf/temp/${xh}.smoothwm.nofix fsSurf/temp/${xh}.inflated.nofix
+    mris_sphere -q fsSurf/temp/${xh}.inflated.nofix fsSurf/temp/${xh}.qsphere.nofix
+    cp fsSurf/temp/${xh}.inflated.nofix fsSurf/temp/${xh}.inflated
+
+    # fix topology
+    mris_euler_number fsSurf/temp/${xh}.orig
+    mris_remove_intersection fsSurf/temp/${xh}.orig fsSurf/temp/${xh}.orig
+    mris_smooth -nw fsSurf/temp/${xh}.orig fsSurf/temp/${xh}.smoothwm
+    mris_inflate fsSurf/temp/${xh}.smoothwm fsSurf/temp/${xh}.inflated
+done
---
+++ flatmountPrep.sh
@@ -0,0 +1,97 @@
+#!bin/bash
+
+# flatmountPrep.sh prepares multiple T1-weighted scans for cortical surface reconstruction 
+# and flatmount generation in FreeSurfer. Toolkits from FSL, FreeSurfer, AFNI are all used and should be installed. 
+# It is likely necessary  to check @animal_warper's output segmentation file 
+# and perform manual adjustments using ITK-SNAP before proceeding to the flatmoutFS scripts.
+
+# Usage: place this script in same directory as the subject's T1-weighted MRI folder, and the folder containing the NIH NMT v2.0 template. 
+
+# Acknowledgements: This is an adaptation of instructions provided by
+# Chris Klink, PhD, the Primate Resource Exchange (Prime-RE), and most recent 
+# updates posted by Rober Boshra, PhD, for utilization of the NMT v2.
+# https://github.com/VisionandCognition/NHP-Freesurfer.git
+# https://github.com/boshra/NHP_freesurfer.git
+
+# The prior instructions involve making individual subject directories within the 
+# template directory, in this case, the NMT v2.0 sym 05mm. This script will 
+# follow this architecture, with outputs ending up in a "single_subjects_scan" folder
+# inside the template directory. << you will see reference to this at the bottom of this script.
+
+# Before starting, there should be a folder in the starting directory
+# titled with the same name as $SUBJ variable below, and containing a subfolder
+# named 'T1s' that has the T1-weighted scans, **in NIFTI format**.
+# If scans are still in DICOM, perform conversion in the T1s folder to NIFTI using AFNI's dcm2niix_afni command. 
+
+# IT'S RECOMMENDED TO TAKE A COUPLE MOMENTS AND USE SOMETHING LIKE FSLEYES AT EACH OF 
+# STEPS WITHIN THE USER INPUT SECTION BELOW TO SEE WHAT YOUR SCANS LOOK LIKE!
+
+# Set up subject variables and paths; Enter subject name and directory name holding the template
+SUBJ=BONNIE # Subject name
+startpath=$(pwd) # Current working directory that holds the T1s folder
+Template=NMT_v2.0_sym_05mm # Variable specifying the template name (folder containing the template should also have this name. This is how it is downloaded from AFNI)
+NMT_path=${startpath}/${Template} # NMT 0.5mm used in this project to avoid voxel resampling
+#####################BEGIN#####################
+# ***NOTE: Scans should be practically identical, very tough to see any differences between. 
+# This script was written with the assumption that all scans are roughly identical to begin with,
+# so averaging will just provide better signal. If individual scans are much different, a manual registration
+# or alignment process of the individual scans to each other may be necessary before averaging.
+
+
+cd ${startpath}/${SUBJ}/T1s # change to T1s directory
+numScans=find . -maxdepth 1 -type f | wc -l # get number of scans
+scans=($(pwd)/*.nii) # make list of scans
+
+fslmaths "${scans[0]}" temp_sum # make temp variable to compute on
+
+#loop through additional scans and add
+for ((i=1; i<numScans; i++)); do
+    fslmaths temp_sum -add "${scans[i]}" temp_sum
+done
+#
+
+fslmaths temp_sum -div ${numScans} ${SUBJ}_avg.nii.gz # divide by number of scans to get average
+rm temp_sum.nii.gz # clean up temp file
+
+######################USER INPUTS & USEFUL PRE-WARPING STEPS########################
+# Following averaing, the user will check the image and get 
+# coordintes that will roughly constraint the field of view (FOV) to that of the NMT. 
+# The coordinates will then be used to crop the image similarly to what is seen in the NMT
+# allowing for @animal_warper's processes to most effectively capture the subject brain.
+
+echo "Averaging of T1s complete. Inspect Image to acquire coordinates for cropping." # Prompt user to inspect the averaged image
+read -p "Ready to continue? Enter cropping coordinates as: Xmin Xmax Ymin Ymax Zmin Zmax > " # Wait for user input
+coords=($REPLY) # Read user input into an array
+
+# crop image to similar FOV as that of the NMT or other template. Takes 'coords' variable
+fslroi ${SUBJ}_avg.nii.gz ${SUBJ}_avg_crop.nii.gz ${coords[0]} ${coords[1]} ${coords[2]} ${coords[3]} ${coords[4]} ${coords[5]}
+# Run basic intensity normalization to roughly match voxel intensity values of the NMT
+fslmaths ${SUBJ}_avg_crop.nii.gz -inm 355 ${SUBJ}_avg_crop_norm.nii.gz # 355 is the mean intensity of the NMT v2.0 sym 05 template
+
+echo "Image cropping and normalization done. Check FOV now if you'd like. Next step: @animal_warper." # Prompt user that cropping and normalization is complete
+# provies an opportunity to check the cropped and normalized image.
+read -p "Ready to continue? Press Enter to run @animal_warper." # Wait for user input
+
+######################@ANIMAL_WARPER########################
+# Run @animal_warper on cropped/normalized image. In this case, settings coded below 
+# involve using the NMT v2.0 sym 05mm template, skull stripping according to the inverse warped NMT
+# brain mask, atlas utilization of the CHARM level 5 (which is also within the NMT template direcoty), 
+# and segmentation specified using the NMT v2.0 sym 05mm segmentation file. 
+# It is important to include this segmentation as the grey/white matter masks will be used in freesurfer.
+# first go back to the start path
+cd ${startpath}
+# make an animal warper output directory
+mkdir -p "${NMT_path}/single_subject_scans/${SUBJ}/AW"
+# run @animal_warper
+@animal_warper \
+    -input ${SUBJ}_avg_crop_norm.nii.gz \
+    -base "${NMT_path}/${Template}.nii.gz" \
+    -outdir "${NMT_path}"/single_subject_scans/${SUBJ}/AW/ \
+    -skullstrip "${NMT_path}/${Template}_brainmask.nii.gz" \
+    -atlas "${NMT_path}/supplemental_CHARM/CHARM_5_in_${Template}.nii.gz" \
+    -seg_followers "${NMT_path}/${Template}_segmentation.nii.gz" \
+    -input_abbrev ${SUBJ} \
+    -base_abbrev NMT_v2 \
+    -atlas_abbrevs CHARM5 \
+    -seg_abbrevs SEG \
+    -align_centers_meth cm
